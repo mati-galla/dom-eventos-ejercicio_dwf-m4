7ac8ca0de01a15a4d2301f5f359819fd7e7cbdd9
mati-galla <matias.jgallardo@gmail.com>
2023-03-01 01:01:41 -0300

    segundo-commit desafio-completado

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function main() {}
+var horizontalPos = 0;
+var verticalPos = 0;
+const redSquare = document.querySelector(".cuadrado-rojo");
+
+function keyDownHandler(e) {
+  console.log(e.keyCode);
+  switch (e.keyCode) {
+    case 37:
+      horizontalPos -= 5;
+      break;
+    case 38:
+      verticalPos -= 5;
+      break;
+    case 39:
+      horizontalPos += 5;
+      break;
+    case 40:
+      verticalPos += 5;
+      break;
+  }
+  redSquare.style.left = horizontalPos + "px";
+  redSquare.style.top = verticalPos + "px";
+}
+
+function main() {
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+}
 
 main();
